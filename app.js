91ad32d744b57908d51079ebf115ff0c03a2549a
Mutasim Billah <mutasim18@student.sust.edu>
2024-02-27 13:53:19 +0600

    parcel working

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {}, "Hello meu");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
